00947173b2107ecf32f519afd468236606839131
郑文勋 <zhengwxthu@163.com>
2017-03-24 23:42:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 #!/opt/anaconda3/bin/python
+# -*- coding:utf-8 -*-
+
 
 from model import KerasModel
 from sockDataGenerator import sockDataGenerator
@@ -6,25 +8,25 @@ from sockDataGenerator import sockDataGenerator
 def train(sp=-1):
     model = KerasModel()
     
-    train = sockDataGenerator(9009,  2)
-    test = sockDataGenerator(9090, 2)
+    train = sockDataGenerator(9009,  50)
+    test = sockDataGenerator(9090, 32)
     next(train)
     next(test)
     print('socket esteblish')
     if sp != -1:
-        chkp = '.' + os.sep + 'models' + os.sep + 'save_' + str(sp) + '.h5'
+        chkp = '.\\models\\save_' + str(sp) + '.h5'
         model.load_weights(chkp)
     print('start point: %d'%sp)
 
-    for i in range(sp + 1, 100):
+    for i in range(sp + 1, 10):
         model.fit_generator(
             generator=train,
-            steps_per_epoch=500,
-            epochs=5,
+            samples_per_epoch=1000,
+            nb_epoch=5,
             validation_data=test,
-            validation_steps=10,
+            nb_val_samples=128
         )
-        model.save_weights('.' + os.sep + 'models' + os.sep + 'save_' + str(i) + '.h5' )
+        model.save_weights('.\\models\\save_' + str(i) + '.h5' )
 
 if __name__ == '__main__':
     train(-1)
