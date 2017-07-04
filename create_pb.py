e134bfc4bd1d0c2c0085be15bb7d581819b08883
zhenchen@tsinghua.edu.cn <zhenchen@tsinghua.edu.cn>
2017-07-04 13:13:43 +0800

    Update create_pb.py

--- create_pb.py
+++ create_pb.py
@@ -1,4 +1,3 @@
-
 from model import KerasModel
 from tensorflow.python.framework import graph_io
 from tensorflow.python.training import saver
@@ -7,18 +6,21 @@ from keras import backend as K
 model = KerasModel()
 sp = 64
 chkp = '.' + os.sep + 'models' + os.sep + 'save_' + str(sp) + '.h5'
-model.load_weights(chkp)
+
 
 sess = K.get_session()
+model.load_weights(chkp)
 ckpt_path = saver.Saver().save(sess, "models/model.ckpt")
 graph_io.write_graph(sess.graph, './models', 'model.pb')
 
 
-```
+'''
+In Android:
+OUT_NAME='Softmax' => OUT_NAME='dense_2/Softmax'
 Run Python: 
 python -m tensorflow.python.tools.freeze_graph 
 --input_graph=./models/model.pb 
 --input_checkpoint=./models/model.ckpt
 --output_graph=./model.pb 
 --output_node_names="dense_2/Softmax"
-```
+'''
