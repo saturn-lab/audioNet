5a74af8a9edc3a8930ba24349956b4a529342b02
郑文勋 <zhengwxthu@163.com>
2017-11-04 13:59:50 +0800

    encoding option should be before filenames

--- augmentation/soxAlter.py
+++ augmentation/soxAlter.py
@@ -56,11 +56,8 @@ def _norm():
     chain += 'norm %s'%dbL
     return chain
 
-def _encoding():
-    return ' -r 16k -e signed -b 16 '
-    
 def soxAlter(filename, outname):
-    chain = ' -V1 %s  %s'%(filename, outname)
+    chain = ' -r 16k -e signed -b 16 -V1 %s  %s'%(filename, outname)
     chain = SOX_PATH + chain
     
     chain += _pitch()
@@ -69,7 +66,7 @@ def soxAlter(filename, outname):
     chain += _echo()
     chain += _bandpass()
     chain += _norm()
-    chain += _encoding()
+    
     # execute
     return subprocess.call(chain, shell=True)
 
